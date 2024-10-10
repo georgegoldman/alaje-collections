147ee1c5bb4487f7cbf5d01aef8077d8289108e4
georgegoldman <georgegoldmanjohn.o@gmail.com>
2024-10-10 20:46:37 +0100

    added userStatus

---
+++ src/main/java/com/hydrogenhr/persistence/entity/UserStatus.java
@@ -0,0 +1,35 @@
+package com.hydrogenhr.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity(name="UserStatus")
+@Table(name = "user_status")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserStatus extends BaseEntity {
+
+    @Column(name = "registered")
+    private boolean registered;
+
+    @Column(name = "mobile_number_verified")
+    private boolean mobileNumberVerified;
+
+    @Column(name = "email_verified")
+    private boolean emailVerified;
+
+    @Column(name = "onboarding_completed")
+    private boolean onboardingCompleted;
+
+    @JoinColumn(name = "user_fk")
+    @OneToOne(fetch = FetchType.EAGER)
+    private User user;
+
+}
